8359d2da36c381b34063a3b4a2055c0a4bb9afe5
TXT-89 <txt.hackathon@TXTs-MacBook-Pro-5.local>
2023-07-14 13:04:43 -0700

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import MapView ,{Marker} from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import { Toolbar } from 'react-native-ios-kit';
+import * as Location from 'expo-location';
 
 export default function App() {
+  {/*Location data */}
+  const [mapRegion,setMapRegion] = useState(null);
+  const userLocation = async () =>{
+    let{status} = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted'){
+      setErrorMsg('Permission to access location was denied');
+    }
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+    setMapRegion
+  }
   return (
+    
+    
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Tool bar at the bottom */}
+      <Toolbar
+  items={[
+    {
+      icon: 'ios-arrow-back',
+      onPress: this.runAction,
+    },
+    {
+      icon: 'ios-arrow-forward',
+      onPress: this.runAction,
+    },
+    {
+      icon: 'ios-refresh',
+      onPress: this.runAction,
+    },
+    {
+      icon: 'ios-share-outline',
+      onPress: this.runAction,
+      disabled: true,
+    },
+  ]}
+/>
+{/*Map */}
+      <MapView style={styles.map} region ={mapRegion}>
+        <Marker coordinate={mapRegion} title ='Marker'/>
+      </MapView>
     </View>
   );
 }
@@ -13,8 +53,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '94%',
   },
 });
