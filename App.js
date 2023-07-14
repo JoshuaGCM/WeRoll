f3f7392d6d364af4ac39c63fd6e71bf0dc2327dd
TXT-89 <txt.hackathon@TXTs-MacBook-Pro-5.local>
2023-07-14 16:17:20 -0700

    second commit

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
 import React,{useState,useEffect} from 'react';
 import MapView ,{Marker} from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View,Button } from 'react-native';
 import { Toolbar } from 'react-native-ios-kit';
 import * as Location from 'expo-location';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
-  {/*Location data */}
-  const [mapRegion,setMapRegion] = useState(null);
+
+  {/*Location data initially*/}
+  let [mapRegion,setMapRegion] = useState({
+    latitude: 34.034411637144196,
+    longitude: -118.45671197410529,
+    latitudeDelta: 0.0922,
+    longitudeDelta:0.0421,
+  });
+  {/*Log of initial data*/}
+  console.log('Initial lat', mapRegion.latitude)
+  console.log('Initial Long', mapRegion.longitude)
+
+
+  const [location,setLocation] = useState(null);
+{/*Request user location info*/}
   const userLocation = async () =>{
     let{status} = await Location.requestForegroundPermissionsAsync();
     if (status !== 'granted'){
       setErrorMsg('Permission to access location was denied');
     }
-    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
-    setMapRegion
+    {/*Gets current position and then sets the latitude and longitude*/}
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true, timeInterval: 1});
+     
+    setLocation(location);
+    setMapRegion({ 
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+    console.log('This is lat',location.coords.latitude)
+    console.log('This is long',location.coords.longitude)
   }
+  useEffect(() =>{
+    userLocation();
+  },[]);
+{/*Return function  */}
   return (
-    
-    
 
     <View style={styles.container}>
       {/*Tool bar at the bottom */}
@@ -46,6 +74,7 @@ export default function App() {
       <MapView style={styles.map} region ={mapRegion}>
         <Marker coordinate={mapRegion} title ='Marker'/>
       </MapView>
+      
     </View>
   );
 }
